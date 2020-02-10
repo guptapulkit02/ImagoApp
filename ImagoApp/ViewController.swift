2bded3332a5a8635cd586b4d19f8303ab2b4895e
Pulkit <>
2020-02-10 15:25:30 +0530

    Xcode proj files added for PoC Single View App

---
+++ ImagoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagoApp
+//
+//  Created by Infy MOBL on 2/10/20.
+//  Copyright © 2020 Wayout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
